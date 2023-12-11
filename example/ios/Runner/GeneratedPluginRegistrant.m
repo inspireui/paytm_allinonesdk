64dc84975e0d62cfe920832437ad30d92f4e9e4d
Le Trung Son <lesonlhld@gmail.com>
2023-12-11 17:10:51 +0700

    feat(Core): Init project

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<paytm_allinonesdk/AllInOneSdkPlugin.h>)
+#import <paytm_allinonesdk/AllInOneSdkPlugin.h>
+#else
+@import paytm_allinonesdk;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AllInOneSdkPlugin registerWithRegistrar:[registry registrarForPlugin:@"AllInOneSdkPlugin"]];
+}
+
+@end
